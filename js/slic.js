33ab4650c983fb132e2b02f7c80c80313460ad9a
Пользователь <suhrob.coder0604@gmail.com>
2023-09-15 12:06:08 +0300

    finish

--- js/slic.js
+++ js/slic.js
@@ -1,23 +1,22 @@
-$(document).ready(function(){
-
-  $('.slider__list').slick({
+$(document).ready(function () {
+  $(".slider__list").slick({
     dots: true,
-    arrows:false,
-    autoplaySpeed:3000,
+    arrows: false,
+    autoplaySpeed: 3000,
     infinite: true,
     speed: 1500,
-    autoplay:true,
+    autoplay: true,
     fade: true,
   });
 
-  $('.useful_resources__list').slick({
+  $(".useful_resources__list").slick({
     dots: true,
-    arrows:false,
+    arrows: false,
     infinite: true,
-    autoplaySpeed:5000,
+    autoplaySpeed: 5000,
     speed: 1000,
-    autoplay:true,
-    slidesToShow: 3, 
+    autoplay: true,
+    slidesToShow: 3,
     slidesToScroll: 3,
     responsive: [
       {
@@ -26,8 +25,8 @@ $(document).ready(function(){
           slidesToShow: 2,
           slidesToScroll: 2,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
 
       {
@@ -36,32 +35,31 @@ $(document).ready(function(){
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
-    ]
+    ],
   });
 
-  $('.owl-carousel').owlCarousel({
-    loop:false,
-    margin:20,
-    data:false,
-    nav:true,
-    autoplaySpeed:2000,
-    autoplay:true,
-      responsive:{
-        0:{
-          items:1
-        },
-  
-        750:{
-          items:2
-        },
-  
-        1250:{
-          items:3
-        }
-    }
-  });
+  $(".owl-carousel").owlCarousel({
+    loop: false,
+    margin: 20,
+    data: false,
+    nav: true,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
 
-})
+      750: {
+        items: 2,
+      },
+
+      1250: {
+        items: 3,
+      },
+    },
+  });
+});
